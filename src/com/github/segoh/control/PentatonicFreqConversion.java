266e22808ab0c831a6cd3f8534fa5685d70e0d25
Sebastian Gutsfeld <sebastian.gutsfeld@freiheit.com>
2012-03-28 23:21:10 +0200

    pentatonic frequency conversion with equal distance between each step

--- src/com/github/segoh/control/PentatonicFreqConversion.java
+++ src/com/github/segoh/control/PentatonicFreqConversion.java
@@ -1,34 +1,29 @@
 package com.github.segoh.control;
 
+
 public class PentatonicFreqConversion implements FreqConversion {
 
     private final int _min;
-    private final int _max;
+    private final int _steps;
     private final int [] _scale;
 
     public PentatonicFreqConversion() {
-        _min = 20;
-        _max = 70;
-        _scale = new int[] { 3, 5, 7, 10 };
+        _min = 36;
+        _steps = 11;
+        _scale = new int[] { 0, 3, 5, 7, 10 };
     }
 
-    private int quantize(final int note) {
-        final int octaves = (int)(note / 12.0) - 1;
-        final int reminder = note % 12;
-
-        int quantized = 0;
-        for (int s : _scale) {
-            if (reminder > s) {
-                quantized = s;
-            } else {
-                break;
-            }
-        }
-        return _min + (octaves * 12) + quantized;
+    public PentatonicFreqConversion(final int steps) {
+        _min = 30;
+        _steps = steps;
+        _scale = new int[] { 0, 3, 5, 7, 10 };
     }
 
     public float toFreq(final float value) {
-        final int note = (int)(_min + (_max - _min + 1) * value);
-        return Conversion.midiToFreq(quantize(note));
+        final int index = (int)Math.floor(value * _steps);
+        final int octave = (int)Math.floor(index / _scale.length);
+        final int reminder = index % _scale.length;
+        final int note = _min + (12 * octave + _scale[reminder]);
+        return Conversion.midiToFreq(note);
     }
 }
