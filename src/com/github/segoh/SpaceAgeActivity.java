93e4cab8769539034602a62ee33f12ee5b524fc7
Sebastian Gutsfeld <sebastian.gutsfeld@freiheit.com>
2012-03-28 23:19:39 +0200

    don't use accelerometer for setting the filter cutoff frequency

--- src/com/github/segoh/SpaceAgeActivity.java
+++ src/com/github/segoh/SpaceAgeActivity.java
@@ -1,10 +1,6 @@
 package com.github.segoh;
 
 import android.app.Activity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,14 +9,9 @@ import android.view.View.OnTouchListener;
 import com.github.segoh.control.FreqConversion;
 import com.github.segoh.control.PentatonicFreqConversion;
 
-
-public class SpaceAgeActivity extends Activity implements SensorEventListener {
-
-    private static final float MAX_ACCELEROMETER_RANGE = 4;
+public class SpaceAgeActivity extends Activity {
 
     private final Synth _synth = new Synth();
-    private SensorManager _sensorManager = null;
-    private long _lastSensorUpdate = -1;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -34,14 +25,12 @@ public class SpaceAgeActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         _synth.start();
-        startAccelerometer();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         _synth.stop();
-        stopAccelerometer();
     }
 
     private void setUpControls(final View view) {
@@ -70,48 +59,5 @@ public class SpaceAgeActivity extends Activity implements SensorEventListener {
                 return true;
             }
         });
-    }
-
-    private void startAccelerometer() {
-        _sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        final Sensor accelerometer = _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        boolean isAccelerometerSupported = _sensorManager.registerListener(
-                this,
-                accelerometer,
-                SensorManager.SENSOR_DELAY_GAME);
-        if (!isAccelerometerSupported) {
-            _sensorManager.unregisterListener(this, accelerometer);
-        }
-    }
-
-    private void stopAccelerometer() {
-        final Sensor accelerometer = _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        _sensorManager.unregisterListener(this, accelerometer);
-        _sensorManager = null;
-    }
-
-    public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
-    }
-
-    private float normalizeSensorValue(final float eventValue) {
-        float x = -1 * eventValue / MAX_ACCELEROMETER_RANGE;
-        if (x > 1) {
-            x = 1;
-        } else if (x < -1) {
-            x = -1;
-        }
-        x = (x + 1) / 2; // [0 - 1]
-        return x * x;
-    }
-
-    public void onSensorChanged(final SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            final long curTime = System.currentTimeMillis();
-            if (_lastSensorUpdate == -1 || (curTime - _lastSensorUpdate) > 100) {
-                _lastSensorUpdate = curTime;
-                final float eventValue = normalizeSensorValue(event.values[0]);
-                _synth.setCutoff(500 + eventValue * 4500);
-            }
-        }
     }
 }
